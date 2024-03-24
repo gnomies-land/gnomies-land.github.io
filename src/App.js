84cb797b10901a15063ead4d1292a726535cd5eb
Adrián Cancio <ujiktl@gmail.com>
2024-03-24 03:48:47 +0100

    Initial website

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Welcome to Gnomies Land!
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Success on the way!
+        </p>
+        <p>
+          Come back soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/gnomies_land"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit us on Twitter
         </a>
       </header>
     </div>
