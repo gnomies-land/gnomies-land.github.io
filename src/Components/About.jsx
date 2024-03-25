8aae308350b2dedf673cd2e11a41a425250d9785
Adrián Cancio <ujiktl@gmail.com>
2024-03-25 16:14:50 +0100

    Update About and Home components

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,37 @@
 export default function About() {
-    return <h1>About Gnomies Land</h1>;
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">About Gnomies Land</h1>
+      <p className="about-description">
+        Welcome to the enchanting world of Gnomies Land, where every step leads
+        to new adventures and where magic is woven into the very fabric of
+        existence. Our mission is not just to create a community, but to forge
+        bonds that transcend the digital realm, where Gnomies from all corners
+        of the globe can come together to explore, create, and thrive.
+      </p>
+
+      <p className="about-description">
+        At Gnomies Land, we cherish the boundless power of imagination and the
+        limitless potential of creativity. Whether you're an accomplished
+        artist, a fervent collector, or simply an admirer of the whimsical and
+        the wonderful, rest assured, there's a place reserved just for you
+        amidst our verdant lands.
+      </p>
+
+      <p className="about-description">
+        Join us on this extraordinary journey as we embark on a quest to build a
+        world filled with wonder, laughter, and endless possibilities. From
+        forging friendships to unleashing the depths of your creativity, every
+        moment spent in Gnomies Land is a testament to the magic that exists
+        when like-minded souls come together.
+      </p>
+
+      <p className="about-description">
+        So, whether you're here to seek inspiration, connect with fellow
+        Gnomies, or simply lose yourself in the enchanting allure of our realm,
+        know that you're not just a visitor – you're an integral part of what
+        makes Gnomies Land the most magical place on the internet!
+      </p>
+    </div>
+  );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,19 @@
 export default function Home() {
-  return (
-    <header className="App-header">
-      <h2>Welcome to Gnomies Land!</h2>
-      <p>Success on the way!</p>
-      <p>Come back soon.</p>
-      <a
-        className="App-link"
-        href="https://twitter.com/gnomies_land"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visit us on Twitter
-      </a>
-    </header>
-  );
-}
-
+    return (
+      <header className="App-header">
+        <h2>Welcome to Gnomies Land!</h2>
+        <p>Embark on an epic journey with the Gnomies!</p>
+        <p>Adventure awaits, so join us now.</p>
+        <a
+          className="App-link"
+          href="https://twitter.com/gnomies_land"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit us on Twitter
+        </a>
+        <p>Prefer Telegram? Join our community <a className="App-link" href="https://t.me/GnomiesLand" target="_blank" rel="noopener noreferrer">here</a>!</p>
+      </header>
+    );
+  }
+  
