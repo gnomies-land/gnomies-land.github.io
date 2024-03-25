0919be952b4af169063dc58d2647dd8b58a1d1c5
Adrián Cancio <ujiktl@gmail.com>
2024-03-25 15:17:42 +0100

    Refactor code structure and add new components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <main className="App">
+        <Home/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <header className="App-header">
+      <h2>Welcome to Gnomies Land!</h2>
+      <p>Success on the way!</p>
+      <p>Come back soon.</p>
+      <a
+        className="App-link"
+        href="https://twitter.com/gnomies_land"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Visit us on Twitter
+      </a>
+    </header>
+  );
+}
+
+export default Home;
