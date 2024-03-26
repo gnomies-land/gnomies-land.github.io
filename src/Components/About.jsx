bcfdd6c7f151bea5cc1fe55dbd9e7d9897e58315
Adrián Cancio <ujiktl@gmail.com>
2024-03-26 23:30:14 +0100

    Refactor About component text

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,19 +3,19 @@ export default function About() {
     <div className="about-container">
       <h1 className="about-heading">About Gnomies Land</h1>
       <p className="about-description">
-        Welcome to the enchanting world of Gnomies Land, where every step leads
-        to new adventures and where magic is woven into the very fabric of
-        existence. Our mission is not just to create a community, but to forge
-        bonds that transcend the digital realm, where Gnomies from all corners
-        of the globe can come together to explore, create, and thrive.
+        Welcome to the enchanting universe of the Gnomies Land, where magic
+        intertwines with the real world. Our mission is not just to create a
+        community, but to forge bonds that transcend the digital realm, where
+        Gnomies from all corners of the globe can come together to explore,
+        create, and thrive.
       </p>
 
       <p className="about-description">
         At Gnomies Land, we cherish the boundless power of imagination and the
         limitless potential of creativity. Whether you're an accomplished
         artist, a fervent collector, or simply an admirer of the whimsical and
-        the wonderful, rest assured, there's a place reserved just for you
-        amidst our verdant lands.
+        the wonderful, rest assured, there's a place reserved just for you in
+        our verdant lands.
       </p>
 
       <p className="about-description">
