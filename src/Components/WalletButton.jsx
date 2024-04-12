d64a46d5c59b9d26c95aac9deff14a53cdaef50b
Adrián Cancio <ujiktl@gmail.com>
2024-04-13 00:34:09 +0200

    Update WalletButton component to handle wallet connection and display appropriate button text

--- src/Components/WalletButton.jsx
+++ src/Components/WalletButton.jsx
@@ -1,14 +1,40 @@
 
 
 export default function WalletButton() {
-    if (window.phantom?.solana?.isPhantom) {
-        return (
-            <div className="wallet-button">Connect Wallet</div>
-        );
+    let provider = getProvider();
+    if (provider) {
+      return (
+        <div
+          className="wallet-button"
+          onClick={handleConnect}>
+          Connect Wallet
+        </div>
+      );
     }
     else {
-        return (
-            <div className="wallet-button">Install Phantom Wallet</div>
-        );
+        return <div className="wallet-button">Connect Wallet</div>
+        
     }
 }
+
+const getProvider = () => {
+    if ('phantom' in window) {
+      const provider = window.phantom?.solana;
+  
+      if (provider?.isPhantom) {
+        return provider;
+      }
+    }
+  
+    window.open('https://phantom.app/', '_blank');
+  };
+
+const handleConnect = async () => {
+  try {
+    const resp = await getProvider().connect();
+    console.log(resp.publicKey.toString());
+    // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo
+  } catch (err) {
+    // { code: 4001, message: 'User rejected the request.' }
+  }
+};
